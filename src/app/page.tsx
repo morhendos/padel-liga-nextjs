4c349d11c79e8ef272532552f4306161b1de77db
Tomasz Mikolajewicz <morhendos@gmail.com>
2024-12-14 19:28:48 +0200

    Fix Image component import and usage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,28 @@
-// In your return statement, update the header section:
+'use client'
 
-<header className="pt-8 pb-6">
-  <div className="max-w-4xl mx-auto px-4">
-    <div className="relative w-full h-[150px] mx-auto max-w-[600px]">
-      <Image 
-        src="/logo.svg"
-        alt="Battle Padel"
-        priority
-        fill
-        style={{ objectFit: 'contain' }}
-      />
+import React, { useState } from 'react';
+import Image from 'next/image';
+
+export default function PadelLeagueApp() {
+  // ... rest of your existing code ...
+
+  return (
+    <div className="min-h-screen bg-[#E8E6E3]">
+      <header className="pt-8 pb-6">
+        <div className="max-w-4xl mx-auto px-4">
+          <div className="relative w-full h-[150px] mx-auto max-w-[600px]">
+            <Image 
+              src="/logo.svg"
+              alt="Battle Padel"
+              priority
+              fill
+              style={{ objectFit: 'contain' }}
+            />
+          </div>
+        </div>
+      </header>
+
+      {/* Rest of your component */}
     </div>
-  </div>
-</header>
+  );
+}
