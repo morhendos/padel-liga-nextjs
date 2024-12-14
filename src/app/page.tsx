26fb495763fc4da928fdf964e2aafc2dc6be526e
Tomasz Mikolajewicz <morhendos@gmail.com>
2024-12-14 19:14:54 +0200

    Update page to use SVG logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 
 export default function PadelLeagueApp() {
-  // ... rest of your state code remains the same ...
+  // ... previous state code ...
 
   return (
     <div className="min-h-screen bg-[#E8E6E3]">
@@ -12,10 +12,10 @@ export default function PadelLeagueApp() {
       <header className="pt-8 pb-6">
         <div className="max-w-4xl mx-auto px-4">
           <Image 
-            src="/logo.png" 
+            src="/logo.svg" 
             alt="Battle Padel" 
             width={400} 
-            height={180}
+            height={100}
             className="mx-auto"
             priority
           />
